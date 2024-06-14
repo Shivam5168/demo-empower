ba4861095a393855ec6848caeddd424576254e25
Shivam5168 <Shivam5168>
2024-06-14 05:26:32 +0000

    Pushed from Locofy Builder

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,90 @@
+import { FunctionComponent } from "react";
+import styles from "./LandingPage.module.css";
+
+const LandingPage: FunctionComponent = () => {
+  return (
+    <div className={styles.landingPage}>
+      <img className={styles.image54Icon} alt="" src="/image-54@2x.png" />
+      <img className={styles.image63Icon} alt="" src="/image-63@2x.png" />
+      <img className={styles.image63Icon} alt="" src="/image-63@2x.png" />
+      <img
+        className={styles.pptMesaDeTrabajo1Copy24}
+        alt=""
+        src="/ppt-mesa-de-trabajo-1-copy-2-4@2x.png"
+      />
+      <img
+        className={styles.pptMesaDeTrabajo1Copy22}
+        alt=""
+        src="/ppt-mesa-de-trabajo-1-copy-2-2@2x.png"
+      />
+      <img className={styles.image106Icon} alt="" src="/image-106@2x.png" />
+      <img className={styles.slide151} alt="" src="/slide-15-1@2x.png" />
+      <img
+        className={styles.electricVehiclePpt1Icon}
+        alt=""
+        src="/electricvehicle-ppt-1@2x.png"
+      />
+      <img
+        className={styles.electricVehiclePpt2Icon}
+        alt=""
+        src="/electricvehicle-ppt-2@2x.png"
+      />
+      <img
+        className={styles.electricVehiclePpt3Icon}
+        alt=""
+        src="/electricvehicle-ppt-3@2x.png"
+      />
+      <img className={styles.landingPageChild} alt="" src="/vector-739.svg" />
+      <img
+        className={styles.landingPageItem}
+        alt=""
+        src="/group-1000005733.svg"
+      />
+      <div className={styles.teenyiconstickCircleSolid} />
+      <div className={styles.groupParent}>
+        <div className={styles.groupWrapper}>
+          <div className={styles.empoweringSustainableParent}>
+            <div
+              className={styles.empoweringSustainable}
+            >{`EMPOWERING SUSTAINABLE `}</div>
+            <b className={styles.freightMobility}>FREIGHT MOBILITY</b>
+          </div>
+        </div>
+        <div className={styles.frameChild} />
+        <div className={styles.selectYourRoleParent}>
+          <b className={styles.selectYourRole}>Select your role</b>
+          <div className={styles.component89Parent}>
+            <div className={styles.component89}>
+              <div className={styles.component89Child} />
+              <div className={styles.carriersParent}>
+                <div className={styles.carriers}>Carriers</div>
+                <img className={styles.frameItem} alt="" src="/vector-42.svg" />
+              </div>
+            </div>
+            <div className={styles.component90}>
+              <div className={styles.component90Child} />
+              <div className={styles.chargingDepotsParent}>
+                <div className={styles.carriers}>Charging Depots</div>
+                <img className={styles.frameItem} alt="" src="/vector-42.svg" />
+              </div>
+            </div>
+            <div className={styles.component91}>
+              <div className={styles.component91Child} />
+              <div className={styles.utilitiesParent}>
+                <div className={styles.carriers}>Utilities</div>
+                <img className={styles.frameItem} alt="" src="/vector-42.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img
+        className={styles.landingPageInner}
+        alt=""
+        src="/group-1000006146.svg"
+      />
+    </div>
+  );
+};
+
+export default LandingPage;
